00a5c4ca3c1e4059aba5cf4a9d7c5a95b123eee7
StackTon <stanoev1337@gmail.com>
2018-08-07 16:37:48 +0300

    implement dynamic rendering to all wallets form localstorage

--- src/components/ListAllMultWallets/ListAllMultWalletsPage.jsx
+++ src/components/ListAllMultWallets/ListAllMultWalletsPage.jsx
@@ -1,31 +1,60 @@
 import React, { Component } from 'react';
 
 class ListAllMultWallets extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            wallets: []
+        }
+    }
+
+    componentDidMount() {
+        let wallets = []
+        for (let key in localStorage) {
+            if (key === "length") {
+                break;
+            }
+            let obj = {
+                "address": key,
+                "balance": localStorage.getItem(key)
+            };
+
+            wallets.push(obj);
+        }
+
+        this.setState({ wallets })
+
+        // bind
+
+        this.walletDelete = this.walletDelete.bind(this);
+        this.walletDetails = this.walletDetails.bind(this);
+    }
+
+    walletDetails() {
+        //TODO
+    }
+
+    walletDelete() {
+        //TODO
+    }
+
     render() {
         return (
             <div>
                 <h1>List All Mult Sig Wallets</h1>
                 <section>
-                    <article>
-                        <div>address: 0x35792029777427920ce7aDecccE9e645465e9C72</div>
-                        <div>balance: 53.5123123 Ether</div>
-                    </article>
-                    <article>
-                        <div>address: 0x35792029777427920ce7aDecccE9e645465e9C72</div>
-                        <div>balance: 53.5123123 Ether</div>
-                    </article>
-                    <article>
-                        <div>address: 0x35792029777427920ce7aDecccE9e645465e9C72</div>
-                        <div>balance: 53.5123123 Ether</div>
-                    </article>
-                    <article>
-                        <div>address: 0x35792029777427920ce7aDecccE9e645465e9C72</div>
-                        <div>balance: 53.5123123 Ether</div>
-                    </article>
-                    <article>
-                        <div>address: 0x35792029777427920ce7aDecccE9e645465e9C72</div>
-                        <div>balance: 53.5123123 Ether</div>
-                    </article>
+                    {this.state.wallets.map((obj) => {
+                        return (
+                            <article key={obj.address}>
+                                <div>address: {obj.address}</div>
+                                <div>balance: {obj.balance} Ether</div>
+                                <button onClick={this.walletDetails}>Details</button>
+                                <button onClick={this.walletDelete}>Delete</button>
+                            </article>
+                        )
+                    })}
+
                 </section>
             </div>
         );
