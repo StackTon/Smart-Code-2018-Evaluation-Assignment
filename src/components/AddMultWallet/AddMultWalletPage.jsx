74072457e3d2347df4fff1f405db1fbf2730f0c7
StackTon <stanoev1337@gmail.com>
2018-08-07 14:03:38 +0300

    implement static view for AddMultWalletPage.jsx and ListAllMultWalletsPage.jsx

--- src/components/AddMultWallet/AddMultWalletPage.jsx
+++ src/components/AddMultWallet/AddMultWalletPage.jsx
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import Input from '../common/Input';
 
 class AddMultWallet extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            address: ""
+        }
+
+        //binding
+        this.onSubmitHandler = this.onSubmitHandler.bind(this);
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+    }
+
+    onChangeHandler(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmitHandler() {
+        //TODO check if address is valid
+
+        //TODO implment add wallet form eth blockchain 
+
+        //TOOD if the address is not for mut sig wallet display error
+    }
+
     render() {
         return (
             <div>
-                AddMultWallet
+                <h2>Add Mult Sig Wallet</h2>
+                    <form onSubmit={this.onSubmitHandler}>
+                        <Input
+                            name="address"
+                            value={this.state.address}
+                            onChange={this.onChangeHandler}
+                            label="Address"
+                        />
+                        <input className="btn btn-outline-primary" type="submit" value="Add" />
+                    </form>
             </div>
         );
     }
--- src/components/ListAllMultWallets/ListAllMultWalletsPage.jsx
+++ src/components/ListAllMultWallets/ListAllMultWalletsPage.jsx
@@ -4,7 +4,29 @@ class ListAllMultWallets extends Component {
     render() {
         return (
             <div>
-                ListAllMultWallets
+                <h1>List All Mult Sig Wallets</h1>
+                <section>
+                    <article>
+                        <div>address: 0x35792029777427920ce7aDecccE9e645465e9C72</div>
+                        <div>balance: 53.5123123 Ether</div>
+                    </article>
+                    <article>
+                        <div>address: 0x35792029777427920ce7aDecccE9e645465e9C72</div>
+                        <div>balance: 53.5123123 Ether</div>
+                    </article>
+                    <article>
+                        <div>address: 0x35792029777427920ce7aDecccE9e645465e9C72</div>
+                        <div>balance: 53.5123123 Ether</div>
+                    </article>
+                    <article>
+                        <div>address: 0x35792029777427920ce7aDecccE9e645465e9C72</div>
+                        <div>balance: 53.5123123 Ether</div>
+                    </article>
+                    <article>
+                        <div>address: 0x35792029777427920ce7aDecccE9e645465e9C72</div>
+                        <div>balance: 53.5123123 Ether</div>
+                    </article>
+                </section>
             </div>
         );
     }
