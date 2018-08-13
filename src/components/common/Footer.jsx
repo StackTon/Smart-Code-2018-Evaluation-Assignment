62f173ccdbb6047df1c9f10c2799ab73600ef447
StackTon <stanoev1337@gmail.com>
2018-08-13 15:06:20 +0300

    add Footer.jsx and PageNotFound.jsx

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                Smart Code 2018 Evaluation Assignment | @copyright
+            </footer>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/components/common/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PageNotFound extends Component {
+    render() {
+        return (
+            <h1>
+                Error 404: Page not found
+            </h1>
+        );
+    }
+}
+
+export default PageNotFound;
